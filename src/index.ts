f445c75d91cd6195bc336141dcbed88e4c99f996
Daniel Jonathan <daniel@cosmicverse.org>
2022-10-17 10:54:17 -0500

    feat(CM-153): adding in new components

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 /* Copyright (C) 2022, CosmicMind, Inc. <http://cosmicmind.com>. All rights reserved. */
 
-// export {
-// 	TypographySize,
-// 	Display,
-// 	Headline,
-// 	Title,
-// 	Label,
-// 	Body,
-// } from '@/typography'
+export {
+	TypographySize,
+	Display,
+	Headline,
+	Title,
+	Label,
+	Body,
+} from '@/typography'
 
-// export {
-// 	ButtonState,
-// 	ElevatedButton,
-// 	FilledButton,
-// 	TonalButton,
-// 	OutlinedButton,
-// 	TextButton,
-// } from '@/buttons'
+export {
+	ButtonState,
+	ElevatedButton,
+	FilledButton,
+	TonalButton,
+	OutlinedButton,
+	TextButton,
+} from '@/buttons'
 
-// export {
-// 	Row,
-// 	RowCenter,
-// 	Column,
-// } from '@/layout'
+export {
+	Row,
+	RowCenter,
+	Column,
+} from '@/layout'
