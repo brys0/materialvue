606e62c725bfc270e0b4ef3654ba218345962786
Daniel Jonathan <daniel@cosmicverse.org>
2023-02-03 16:43:19 -0600

    feat(CM-199): wip build setup for CI/CD

--- vite.config.test.ts
+++ vite.config.test.ts
@@ -30,62 +30,41 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/// <reference types="vitest" />
+
 import {
 	URL,
 	fileURLToPath,
 } from 'node:url'
 
 import {
-	LibraryFormats,
-	UserConfigExport,
 	defineConfig,
+	UserConfigExport,
 } from 'vite'
 
-import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-
-const external = [
-	'vue',
-	'pinia',
-	'vee-validate',
-	'@cosmicmind/foundationjs'
-]
-
 const srcDir = './src'
-const emptyOutDir = false
-const formats: LibraryFormats[] = [ 'es' ]
+const distDir = './dist'
+const testsDir = '__tests__'
+const benchmarksDir = '__benchmarks__'
 
-export default defineConfig(({ mode }) => {
-	const minify = 'production' === mode
+export default defineConfig(() => {
 	const config: UserConfigExport = {
 		resolve: {
 			alias: {
 				'@': fileURLToPath(new URL(srcDir, import.meta.url)),
 			},
 		},
-		plugins: [
-			vue(),
-			dts(),
-			viteStaticCopy({
-				targets: [
-					{
-						src: `${srcDir}/lib/sass`,
-						dest: './',
-					}
-				],
-			})
-		],
-		build: {
-			emptyOutDir,
-			lib: {
-				name: process.env.npm_package_name,
-				entry: `${srcDir}/lib/vue/index.ts`,
-				formats,
-				fileName: 'lib.es',
+		test: {
+			include: [ `${testsDir}/**/*.spec.ts` ],
+			benchmark: {
+				include: [ `${benchmarksDir}/**/*.bench.ts` ],
+				outputFile: `${distDir}/benchmarks.json`,
 			},
-			rollupOptions: { external },
-			minify,
+			coverage: {
+				provider: 'c8',
+				include: [ '**/src/**' ],
+				extension: [ '.ts' ],
+			},
 		},
 	}
 	return config
